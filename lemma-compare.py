308efc872514d30bfa8ca48edf6ddb54466e41a2
Tom Rainsford <tmr740-ac@yahoo.co.uk>
2023-12-31 19:30:25 +0100

    working on lemma compare

--- lemma-compare.py
+++ lemma-compare.py
@@ -44,6 +44,48 @@ def vote(forms, ignore_numbers=False):
     for option in d:
         if d[option] == maxscore: l.append(option)
     return '|'.join(l)
+    
+def score_lemmas(poss, goldlemmas=[], autolemmas=[], lookup_lemmas=[], lookup_poss=[]):
+    print(goldlemmas, autolemmas, lookup_lemmas, lookup_poss)
+    score = 0
+    # Case 1. There is a gold lemma. Score 0.
+    # Note that gold lemmas shouldn't be ambiguous...
+    if goldlemmas:
+        lemma = '|'.join(goldlemmas)
+    # Case 2a. There is a single, unambiguous lemma from the lookup which 
+    # matches the autolemma.
+    elif lookup_lemmas and len(lookup_lemmas) == 1 and lookup_lemmas[0] in autolemmas:
+        lemma = lookup_lemmas[0]
+        score += 1
+        # But if the lookup_lemma part of speech tag doesn't match, add 1 to score
+        if not lookup_poss[0] in poss:
+            score += 1
+    # Case 2b. There is a single, unambiguous lemma from the lookup but it doesn't
+    # match the autolemma
+    elif lookup_lemmas and len(lookup_lemmas) == 1 and autolemmas:
+        lemma = lookup_lemmas[0]
+        score = 3
+        # If the pos tag doesn't match, add 1
+        if not lookup_poss[0] in poss:
+            score += 1
+    # Case 3. There are multiple lookup lemmas but only one with the correct pos.
+    elif lookup_lemmas and len(set(lookup_poss) & set(poss)) == 1 and \
+    lookup_poss.count((set(lookup_poss) & set(poss)).pop()) == 1:
+        lemma = lookup_lemmas[lookup_poss.index((set(lookup_poss) & set(poss)).pop())]
+        score = 1
+        # If the autolemma doesn't match, add 1 to the score
+        if autolemmas and not lemma in autolemmas:
+            score += 1
+    # Case 3b. There are multiple lookup lemmas with the correct pos, but
+    # only one matches one of the autolemmas.
+    #elif lookup_lemmas and len(set(lookup_poss) & set(poss)) > 1 or \
+    #(len(set(lookup_poss) & set(poss)) == 1 and lookup_poss.count((set(lookup_poss) & set(poss)).pop()) > 1):
+        # TODO
+        #pass
+    else:
+        lemma = 'NOTDONE'
+        score = 100
+    return lemma, score
 
 def disambiguate_autoposlemma(autoposlemmas, outfile='out.txt', ignore_numbers=False):
     # Open the files
@@ -100,7 +142,7 @@ def disambiguate_pos(autoposs, goldposs=[], outfile='out.txt'):
             fout.write(form + '\t' + tag + '\n')
     if goldpos_f: goldpos_f.close()
     for autopos_f in autopos_fs: autopos_f.close()
-
+    
 def main(
     goldpos=[], goldlemma=[], goldposlemma=[], lookupposlemma=[],
     autopos=[], autoposlemma=[], outfile='out.txt',
@@ -138,19 +180,48 @@ def main(
     else:
         autolemmafile = ''
         
+        
     # Step 5. Load lexicon files
     if lookupposlemma:
-        l = [parse_lexicon(x) for x in lookupposlemma]
-        pos_d, lemma_d = l[0][0], l[0][1]
-        for xpos_d, xlemma_d in l[1:]:
+        l = [parse_lexicon(x, ignore_numbers=ignore_numbers) for x in lookupposlemma]
+        lemma_d, pos_d = l[0][0], l[0][1]
+        for xlemma_d, xpos_d in l[1:]:
             pos_d.update({key: pos_d.get(key, []) + xpos_d[key] for key in xpos_d})
             lemma_d.update({key: lemma_d.get(key, []) + xlemma_d[key] for key in xlemma_d})
     else:
         pos_d, lemma_d = {}, {}
-    print(pos_d, lemma_d)
-    
-    
-    
+        
+    # Step 6. Open the files and begin iteration
+    autolemma_f = open(autolemmafile, 'r', encoding='utf-8') if autolemmafile else None
+    goldposlemma_f = open(goldposlemma[0], 'r', encoding='utf-8') if goldposlemma else None
+    with open(posfile, 'r', encoding='utf-8') as fin:
+        with open(outfile, 'w', encoding='utf-8') as fout:
+            for line in fin:
+                line_list = line.rstrip().split('\t') # Read the line
+                form = line_list[0] # Get the form
+                try:
+                    poss = line_list[1].split('|') # Get the pos list
+                except IndexError:
+                    poss = []
+                if autolemma_f: # Get the (list of) autolemmas
+                    al_line = autolemma_f.readline().rstrip()
+                    try:
+                        autolemmas = al_line.split('\t')[2].split('|')
+                    except IndexError:
+                        autolemmas = []
+                if goldposlemma_f: # Get the (list of) gold lemmas
+                    gpl_line = goldposlemma_f.readline().rstrip()
+                    try:
+                        goldlemmas = gpl_line.split('\t')[2].split('|')
+                    except IndexError:
+                        goldlemmas = []
+                if lookupposlemma:
+                    lookup_lemmas = lemma_d.get(form, [])
+                    lookup_poss = pos_d.get(form, [])
+                else:
+                    lookup_lemmas, lookup_poss = [], []
+                lemma, score = score_lemmas(poss, goldlemmas, autolemmas, lookup_lemmas, lookup_poss)
+                fout.write('\t'.join([form, lemma, str(score)]) + '\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
--- lib/common.py
+++ lib/common.py
@@ -2,7 +2,7 @@
 
 # Common functions
 
-def parse_lexicon(fname):
+def parse_lexicon(fname, ignore_numbers=False):
     # For this script, need to parse the file into two lookup
     # dictionaries with key = form
     with open(fname, 'r', encoding='utf-8') as f:
@@ -13,6 +13,12 @@ def parse_lexicon(fname):
             x = line.split('\t')
             if len(x) != 3: continue # ignore malformed lines
             lemma, pos, forms = x[0], x[1], x[2].split('|')
+            if ignore_numbers: # Remove numbers if ignore numbers is enabled
+                s = ''
+                for char in lemma:
+                    if not char.isdigit():
+                        s += char
+                lemma = s
             for form in forms:
                 if form in lemma_d:
                     lemma_d[form].append(lemma)
